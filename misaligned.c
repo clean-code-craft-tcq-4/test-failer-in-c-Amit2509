4563cdc341b585ff8e2abc0c89872bd0f6e61fd8
Amit2509 <amitsrivastava2509@gmail.com>
2022-08-15 00:30:26 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -6,11 +6,10 @@ char* colorMap[];
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            char* buffer;
+            char* buffer[MAX_COLORPAIR_NAME_CHARS];
             sprintf(buffer, "%s | %s", majorColor[i], minorColor[i]);
             colorMap[i * 5 + j] = buffer;
             printf("%d | %s\n", i * 5 + j, colorMap[i * 5 + j] );
