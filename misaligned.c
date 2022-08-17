ebcec739dca5abaa5961da1f5e7e7bbba7d1e41b
Amit2509 <amitsrivastava2509@gmail.com>
2022-08-17 13:35:31 +0530

    Update misaligned.c
    Making all the test cases fail

--- misaligned.c
+++ misaligned.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
-#include <cstring>
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-char* colorMap[25];
+#include <string.h>
+char* colorMapMajor[25] = {};
+char* colorMapMinor[25] = {};
 
 void printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
@@ -10,10 +10,9 @@ void printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            char buffer[MAX_COLORPAIR_NAME_CHARS];
-            sprintf(buffer, "%s | %s", majorColor[i], minorColor[i]);
-            colorMap[i * 5 + j] = buffer;
-            printf("%d | %s\n", i * 5 + j, colorMap[i * 5 + j] );
+            colorMapMajor[i * 5 + j] = majorColor[i];
+            colorMapMinor[i * 5 + j] = minorColor[i];
+            printf("%d | %s | %s\n", i * 5 + j, colorMapMajor[i * 5 + j], colorMapMinor[i * 5 + j] );
         }
     }
     return;
@@ -21,12 +20,8 @@ void printColorMap() {
 
 int main() {
     printColorMap();
-    char buffer[MAX_COLORPAIR_NAME_CHARS];
-    sprintf(buffer, "%s | %s", "White", "Blue");
-    int result = strcmp(colorMap[1], buffer);
-    assert(result == 0);
-    //assert(strcmp(colorMap[1], "White | Blue") == 0);
-    //assert(strcmp(colorMap[2], "| White | Orange") == 0);
+    assert(strcmp(colorMapMajor[1], "White")==0);
+    assert(strcmp(colorMapMinor[1], "Orange")==0);
     printf("All is well (maybe!)\n");
     return 0;
 }
