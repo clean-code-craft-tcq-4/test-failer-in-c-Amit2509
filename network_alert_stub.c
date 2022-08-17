fe5a231d346bd017fb5621a7825e6a0c05f34e3d
Amit2509 <amitsrivastava2509@gmail.com>
2022-08-17 14:41:26 +0530

    Create network_alert_stub.c

---
+++ network_alert_stub.c
@@ -0,0 +1,13 @@
+#include "network_alert_stub.h"
+
+int networkAlertStub(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    if (celcius<=THRESHOLD_TEMP)
+    {
+        return 200;
+    }
+    else
+    {
+        return 500;
+    }
+}
